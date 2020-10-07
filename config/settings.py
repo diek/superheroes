56a693a998e6d6ef389edbd02448152fa1631f7f
diek <d_kearney@bellaliant.net>
2020-10-07 01:41:16 -0300

    Add dotenv and cors headers

--- config/settings.py
+++ config/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -20,12 +23,17 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l%h%k_^$17a59c&)#6vdz$1@z$wo!r#n31_mksdhdlkw1%ucn5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000"
+]
+
 
 DJANGO_APPS = [
     'django.contrib.admin',
@@ -42,12 +50,14 @@ PROJECT_APPS = [
 
 UTIL_APPS = [
     'rest_framework',
-    'django_extensions'
+    'django_extensions',
+    'corsheaders'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + UTIL_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
